3830a1145e7f5fe8a179db32763b63265d4867eb
raf <raf@DESKTOP-PN9HH5P>
2022-05-19 15:46:12 -0300

    Finalizado

--- src/main/java/Main/Jogo.java
+++ src/main/java/Main/Jogo.java
@@ -1,5 +1,4 @@
 package Main;
-import java.util.Random;
 import java.util.Scanner;
 /** 
    por Rafael Ferreira Goulart
@@ -10,9 +9,9 @@ public class Jogo {
     public static boolean jogando = false;
     public static String nomeP1,
                          nomeP2,
-                         strTitulo = "|---------- Tit-Tak-Toe ----------|",
-                         strEntPosP1 = "| P1 - Escolha uma posição: ", 
-                         strEntPosP2 = "| P2 - Escolha uma posição: ";
+                         strTitulo =   "|--------------- Tit-Tak-Toe ---------------|",
+                         strEntPosP1 = "| P1 - Escolha uma posição:                 | ", 
+                         strEntPosP2 = "| P2 - Escolha uma posição:                 | ";
     public static int jogadas = 9;
     public static double quemJoga;
     
@@ -23,6 +22,7 @@ public class Jogo {
     public static void iniciaJogo() {
         double quemComeca;
         int posJogada;
+        boolean ganhou = false;
         
         System.out.println(strTitulo);
         quenheP1();
@@ -33,9 +33,10 @@ public class Jogo {
         quemJoga = quemComeca;
         
         String[] tabuleiro = criaTabuleiro();
+        int[] tab = criaTabuleiroAux();
         
         while (jogadas > 0) { 
-            mostraTabuleiro(tabuleiro);
+            mostraTabuleiro(tabuleiro, tab);
             System.out.println();
             if (jogadas == 9) {
                 tabuleiro = atualizaTabuleiro(tabuleiro);
@@ -44,13 +45,21 @@ public class Jogo {
             posJogada = joga(quemJoga, tabuleiro);
             
             tabuleiro = mandaJogada(tabuleiro, posJogada, jogadas);
+            tab = tabuleiroAux(tab, posJogada, jogadas);
             
-            verificaVencedor(tabuleiro, quemJoga);
+            ganhou = verificaVencedor(tab, quemJoga, jogadas);
             
-            quemJoga = proxJoga(quemJoga);
-            jogadas--;
+            if (ganhou == true) {
+                jogadas = -1;
+            } else {
+                quemJoga = proxJoga(quemJoga);
+                jogadas--;
+            }
         }
-        mostraTabuleiro(tabuleiro);
+        if (jogadas == 0) {
+            System.out.println("| Empate! Ninguém venceu.                   |");
+        }
+        mostraTabuleiro(tabuleiro, tab);
     }
     
     public static String[] criaTabuleiro() {
@@ -60,7 +69,12 @@ public class Jogo {
         return tabuleiro;
     }
     
-    public static void mostraTabuleiro(String[] tabuleiro) {
+    public static int[] criaTabuleiroAux() {
+        int[] tab = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+        return tab;
+    }
+    
+    public static void mostraTabuleiro(String[] tabuleiro, int[] tab) {
         for (int pos = 0; pos < 9; pos++) {
             if (pos == 2 || pos == 5) {
                System.out.print(tabuleiro[pos] + "\n");   
@@ -82,13 +96,13 @@ public class Jogo {
     }
     
     public static String quenheP1() {
-        System.out.println("| P1 - Informe seu nome:");
+        System.out.println("| P1 - Informe seu nome:                    |");
         nomeP1 = ent.nextLine();
         return nomeP1;
     }
     
     public static String quenheP2() {
-        System.out.println("| P2 - Informe seu nome:");
+        System.out.println("| P2 - Informe seu nome:                    |");
         nomeP2 = ent.nextLine();
         return nomeP2;
     }
@@ -100,13 +114,12 @@ public class Jogo {
         } else {
             System.out.println("| Primeiro a jogar: " + nomeP2);
         }
-        System.out.println("Você é \"X\"");
+        System.out.println("| Você é \"X\"                                |");
         return quemComeca;
     }
     
     public static int joga(double quemJoga, String[] tabuleiro) {
         int posJogada;
-        boolean jaFoiEscolhida;
         boolean podePassar;
         
         if (quemJoga == 0) {
@@ -123,21 +136,20 @@ public class Jogo {
     }
     
     public static boolean confereJogada(int posJogada, String[] tabuleiro) {
-        String strPosInvalida = "| Posição Inválida! Deve ser entre 1 e 9 ";
-        String strJaFoiEscolhida = "| Posição já foi escolhida antes! ";
-        String strInsNovamente = "| Insira novamente: ";
+        String strPosInvalida =    "| Posição Inválida! Deve ser entre 1 e 9    |";
+        String strJaFoiEscolhida = "| Posição já foi escolhida antes!           |";
+        String strInsNovamente =   "| Insira novamente:                         |";
         boolean podePassar;
         if (posJogada < 0 || posJogada > 8) {
             System.out.println(strPosInvalida + "\n" + strInsNovamente);
             podePassar = false;
         } else {
-            podePassar = true;
-        }
-        if (jaFoiEscolhida(posJogada, tabuleiro)) {
-            System.out.println(strJaFoiEscolhida + "\n" + strInsNovamente);
-            podePassar = false;
-        } else {
-            podePassar = true;
+            if (jaFoiEscolhida(posJogada, tabuleiro)) {
+                System.out.println(strJaFoiEscolhida + "\n" + strInsNovamente);
+                podePassar = false;
+            } else {
+                podePassar = true;
+            }
         }
         return podePassar;
     }
@@ -156,8 +168,51 @@ public class Jogo {
         return jaFoiEscolhida;
     }
     
-    public static void verificaVencedor(String[] tabuleiro, double quemJoga) {
+    //    |1|2|3|
+    //    |4|5|6|
+    //    |7|8|9|
+    // -------------------------------------------------------------------------
+    //    |0|1|2| /
+    //    |3|4|5| /
+    //    |6|7|8| /
+    
+    public static boolean verificaVencedor(int[] tab, double quemJoga, int jogadas) {
+        boolean ganhou = false;
+        String ganhador;
+        if (quemJoga == 0) {
+            ganhador = nomeP1;
+        } else {
+            ganhador = nomeP2;
+        }
         
+        if (jogadas <= 5) {
+            // Horizontal
+            if        (tab[0] == 0 && tab[1] == 0 && tab[2] == 0 || tab[0] == 1 && tab[1] == 1 && tab[2] == 1) {
+                ganhou = true;
+            } else if (tab[3] == 0 && tab[4] == 0 && tab[5] == 0 || tab[3] == 1 && tab[4] == 1 && tab[5] == 1) {
+                ganhou = true;
+            } else if (tab[6] == 0 && tab[7] == 0 && tab[8] == 0 || tab[6] == 1 && tab[7] == 1 && tab[8] == 1) {
+                ganhou = true;
+            }
+            //Vertical
+            if        (tab[0] == 0 && tab[3] == 0 && tab[6] == 0 || tab[0] == 1 && tab[3] == 1 && tab[6] == 1) {
+                ganhou = true;
+            } else if (tab[1] == 0 && tab[4] == 0 && tab[7] == 0 || tab[1] == 1 && tab[4] == 1 && tab[7] == 1) {
+                ganhou = true;
+            } else if (tab[2] == 0 && tab[5] == 0 && tab[8] == 0 || tab[2] == 1 && tab[5] == 1 && tab[8] == 1) {
+                ganhou = true;
+            }
+            // Diagonal
+            if        (tab[0] == 0 && tab[4] == 0 && tab[8] == 0 || tab[0] == 1 && tab[4] == 1 && tab[8] == 1) {
+                ganhou = true;
+            } else if (tab[2] == 0 && tab[4] == 0 && tab[6] == 0 || tab[2] == 1 && tab[4] == 1 && tab[6] == 1) {
+                ganhou = true;
+            }
+        }
+        if (ganhou) {
+            System.out.println("| "+ ganhador + " Venceu!");
+        }
+        return ganhou;
     }
     
     public static String[] mandaJogada(String[] tabuleiro, int posJogada, int jogadas) {
@@ -175,6 +230,15 @@ public class Jogo {
             }
         }
         return tabuleiro;
+    }
+    
+    public static int[] tabuleiroAux(int[] tab, int posJogada, int jogadas) {
+        if (jogadas%2 == 1) {
+            tab[posJogada] = 0;
+        } else {
+            tab[posJogada] = 1;
+        }
+        return tab;
     }
     
     public static double proxJoga(double quemJoga) {
